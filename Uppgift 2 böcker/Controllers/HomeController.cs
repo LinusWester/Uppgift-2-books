e46a4071a52197cbe746e7edb554a09666786c66
Linus Wester <LWester19@gmail.com>
2021-02-26 16:34:30 +0100

    Lektion 2 uppgifter färdiga

--- Uppgift 2 böcker/Controllers/HomeController.cs
+++ Uppgift 2 böcker/Controllers/HomeController.cs
@@ -32,6 +32,10 @@ namespace Uppgift_2_böcker.Controllers
                 new Books { Id = 4, Title = "Fool's Fate", Author = "Robin Hobb", Info = "Once assassin to the king, Fitz is now Skillmaster to Prince Dutiful's small band, sailing towards the distant Out Island of Aslevjal." }
             };
             var selectedBook = booklist.Where(o => o.Id == Id).FirstOrDefault();
+
+            if (selectedBook == null)
+                return new HttpStatusCodeResult(204);
+
             return new JsonResult { Data = selectedBook, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
         }
 
@@ -48,5 +52,35 @@ namespace Uppgift_2_böcker.Controllers
 
             return new JsonResult { Data = booklist, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
         }
+        
+        [HttpPost]
+        public ActionResult CreateBook(Books book)
+        {
+            IList<Books> booklist = new List<Books>();
+            {
+                booklist.Add(book);
+                return new HttpStatusCodeResult(200);
+            }
+        }
+
+        [HttpDelete]
+        public ActionResult DeleteBook(int Id)
+        {
+            IList<Books> booklist = new List<Books>
+            {
+                new Books { Id = 1, Title = "Hobbit", Author = "J.R.R Tolkien", Info = "The Hobbit is set within Tolkien's fictional universe and follows the quest of home-loving Bilbo Baggins" },
+                new Books { Id = 2, Title = "The lord of the Rings", Author = "J.R.R Tolkien", Info = "The Lord of the Rings is an epic high fantasy novel[ by the English author and scholar J. R. R. Tolkien. Set in Middle-earth," },
+                new Books { Id = 3, Title = "Fool's Errand", Author = "Robin Hobb", Info = "Fifteen years have passed since the end of the Red Ship War with the terrifying Outislanders." },
+                new Books { Id = 4, Title = "Fool's Fate", Author = "Robin Hobb", Info = "Once assassin to the king, Fitz is now Skillmaster to Prince Dutiful's small band, sailing towards the distant Out Island of Aslevjal." }
+            };
+            var selectedBook = booklist.Where(o => o.Id == Id).FirstOrDefault();
+
+            if (selectedBook == null)
+                return new HttpStatusCodeResult(204);
+
+                booklist.Remove(selectedBook);
+                return new HttpStatusCodeResult(200);
+        }
+
     }
 }
